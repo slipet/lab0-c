d1add5bcafbad6e7c4c7f4e94a3d29b92e52e494
slipet <slipet@csie.io>
2023-09-28 14:03:22 +0800

    Add cmd file for experiments and lib/list_sort

---
+++ traces/trace-shuffle.cmd
@@ -0,0 +1,9 @@
+# Test shuffle
+option fail 0
+option malloc 0
+new
+ih a
+ih b
+ih c
+show
+shuffle
---
+++ traces/trace-sort.cmd
@@ -0,0 +1,8 @@
+# Test qsort & list_sort
+option fail 0
+option malloc 0
+new
+ih RAND 1000000
+time
+kernel_sort
+time
