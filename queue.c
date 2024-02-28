04a4da9d238375d118ff16eaac6d45bd4fe46d16
slipet <slipet@github.com>
2024-02-28 20:47:15 +0800

    Add queue reversal functionality
    Implements q_reverse to invert the order of elements within a queue.

--- queue.c
+++ queue.c
@@ -158,37 +158,45 @@ void q_swap(struct list_head *head)
 }
 
 /* Reverse elements in queue */
-void q_reverse(struct list_head *head) {}
-
-/* Reverse the nodes of the list k at a time */
-void q_reverseK(struct list_head *head, int k)
+void q_reverse(struct list_head *head)
 {
-    // https://leetcode.com/problems/reverse-nodes-in-k-group/
-}
+    if (head == NULL || list_empty(head) || list_is_singular(head))
+        return;
+    struct list_head *iter = NULL, *next = NULL;
+    list_for_each_safe (iter, next, head) {
+        list_move(iter, head);
+        return;
+    }
 
-/* Sort elements of queue in ascending/descending order */
-void q_sort(struct list_head *head, bool descend) {}
+    /* Reverse the nodes of the list k at a time */
+    void q_reverseK(struct list_head * head, int k)
+    {
+        // https://leetcode.com/problems/reverse-nodes-in-k-group/
+    }
 
-/* Remove every node which has a node with a strictly less value anywhere to
- * the right side of it */
-int q_ascend(struct list_head *head)
-{
-    // https://leetcode.com/problems/remove-nodes-from-linked-list/
-    return 0;
-}
+    /* Sort elements of queue in ascending/descending order */
+    void q_sort(struct list_head * head, bool descend) {}
 
-/* Remove every node which has a node with a strictly greater value anywhere to
- * the right side of it */
-int q_descend(struct list_head *head)
-{
-    // https://leetcode.com/problems/remove-nodes-from-linked-list/
-    return 0;
-}
+    /* Remove every node which has a node with a strictly less value anywhere to
+     * the right side of it */
+    int q_ascend(struct list_head * head)
+    {
+        // https://leetcode.com/problems/remove-nodes-from-linked-list/
+        return 0;
+    }
 
-/* Merge all the queues into one sorted queue, which is in ascending/descending
- * order */
-int q_merge(struct list_head *head, bool descend)
-{
-    // https://leetcode.com/problems/merge-k-sorted-lists/
-    return 0;
-}
+    /* Remove every node which has a node with a strictly greater value anywhere
+     * to the right side of it */
+    int q_descend(struct list_head * head)
+    {
+        // https://leetcode.com/problems/remove-nodes-from-linked-list/
+        return 0;
+    }
+
+    /* Merge all the queues into one sorted queue, which is in
+     * ascending/descending order */
+    int q_merge(struct list_head * head, bool descend)
+    {
+        // https://leetcode.com/problems/merge-k-sorted-lists/
+        return 0;
+    }
